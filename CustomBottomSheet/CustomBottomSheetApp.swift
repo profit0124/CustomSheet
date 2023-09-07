acc66360711051d2b648dc23f7d7a0f9a66eb8b1
profit0124 <profit0124@gmail.com>
2023-09-07 22:10:42 +0900

    Initial Commit

---
+++ CustomBottomSheet/CustomBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBottomSheetApp.swift
+//  CustomBottomSheet
+//
+//  Created by Sooik Kim on 2023/09/07.
+//
+
+import SwiftUI
+
+@main
+struct CustomBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
