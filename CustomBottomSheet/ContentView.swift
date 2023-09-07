95d568ee3816edb6441c407a516c8e0aa25d8aa2
profit0124 <profit0124@gmail.com>
2023-09-08 00:13:45 +0900

    Basic

--- CustomBottomSheet/ContentView.swift
+++ CustomBottomSheet/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var offset: CGFloat = 0
+    @State var previousOffset: CGFloat = 0
+    @GestureState var dragGestureOffset: CGFloat = 0
+    
+    @State var isPresent: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            Button(action: {
+                withAnimation(.easeInOut(duration: 0.2)){
+                    isPresent.toggle()
+                }
+            }, label: {
+                Text(isPresent ? "Close" : "Open")
+            })
+            
+            
+            // Sheet
+            GeometryReader{ proxy -> AnyView in
+                let height = proxy.frame(in: .global).height
+                return AnyView(
+                    ZStack {
+                        Color.red
+                        
+                        VStack {
+                            Capsule()
+                                .fill(Color.white)
+                                .frame(width: 80, height: 5)
+                                .padding(.top)
+                        }
+                        .padding(.horizontal)
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        
+                        VStack {
+                            // Something Contents
+                        }
+                    }
+                        
+                        .offset(y: isPresent ? height / 2 + 50 : height)
+                        .offset(y: !isPresent ? 0 : offset)
+                        .gesture(
+                            DragGesture()
+                                .updating($dragGestureOffset, body: { value, out, _ in
+                                    out = value.translation.height
+                                    let tempOffset = dragGestureOffset + previousOffset
+                                    if -tempOffset < height / 2 {
+                                        DispatchQueue.main.async {
+                                            self.offset = dragGestureOffset + previousOffset
+                                        }
+                                    }
+                                })
+                                .onEnded({ value in
+                                    withAnimation {
+                                        if -offset > (height / 2 - 50) / 2 {
+                                            offset = -(height / 2)
+                                        } else if  -offset < 0 {
+                                            offset = 0
+                                            self.isPresent.toggle()
+                                        } else {
+                                            offset = 0
+                                        }
+                                    }
+                                    self.previousOffset = self.offset
+                                })
+                        )
+                )
+                
+            }
+            .ignoresSafeArea()
         }
-        .padding()
     }
 }
 
